d00676b5fb22276d269577308015ffcc965dd5f7
Larry Bernstein <1larsb@gmail.com>
2011-11-24 11:23:07 -0800

    Inital commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ProspectFinder::Application.config.secret_token = '923cff9ebb3df2268b46ed48a03a54b10e5cd0d5434f2bbe58377df62087ded0d9e6bd86fca11efba97c5844e81658e21e0bb221da7f728cff0046f98269a66b'
