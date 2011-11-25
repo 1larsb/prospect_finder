f7ee9fde8599a2c7c8ec8648f381e7325c33bf50
Larry Bernstein <1larsb@gmail.com>
2011-11-24 20:56:34 -0800

    Start of main page layout

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,12 +1,19 @@
 require 'spec_helper'
 
 describe PagesController do
+  render_views
 
   describe "GET 'home'" do
     it "returns http success" do
       get 'home'
       response.should be_success
     end
+    
+    it "should have the right title" do 
+      get 'home'
+      response.should have_selector("title", :content => "Prospect Finder | Home")
+    end
   end
 
 end
+
